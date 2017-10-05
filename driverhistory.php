f9989f0dcc6cd753e996ce6f3e50f096e14814fc
wijayaerick <wijaya.erick52@gmail.com>
2017-10-05 13:15:25 +0700

    add driver history ui

---
+++ driverhistory.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="icon" href="icon.png" />
+	<title>Ojek Panas | History</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+	<div id="navbar">
+    	<script src="js/navbar.js" rel="javascript" type="text/javascript"></script>
+    	<div class="after-box">
+      		<ul class="centered">
+						<li class="list-item"><a href="order.php">ORDER</a>
+		        <li class="active"><a href="historyorder.php">HISTORY</a>
+		        <li class="list-item"><a href="profile.php">MY PROFILE</a>
+      		</ul>
+    	</div>
+    </div>
+    <div class="floating-box-left-mo2">TRANSACTIONS HISTORY</div>
+    <div id="mini-navbar">
+	    <table>
+	    	<tr>
+					<td class="mini-navbar"><a href="historyorder.php">MY PREVIOUS ORDERS</a></td>
+	    		<td class="selected-navbar"><a href="driverhistory.php">DRIVER HISTORY</a></td>
+	    	</tr>
+	    </table>
+    </div>
+    <div class="empty-space"></div>
+    <div class="history-list-item">
+    	<table>
+    		<tr>
+    			<td rowspan="6"><img class="square-image" src="fish.png" alt="Driver Profile"></td>
+    			<td rowspan="6" class="horizontal-space"></td>
+    			<td colspan="2" class="history-date">Sunday, September 24th 2017</td>
+    			<td width="100" rowspan="2"><button class="hide-button">HIDE</button></td>
+    		</tr>
+    		<tr>
+    			<td colspan="2" class="history-driver-name">Bomba-rattata</td>
+    		</tr>
+    		<tr>
+    			<td colspan="2" class="history-route">Saffron City-Pewter City</td>
+    		</tr>
+    		<tr>
+                <td colspan="2" class="history-rating">gave <font color="orange">5</font> stars for this order</td>
+            </tr>
+            <tr>
+                <td colspan="2">and left comment:</td>
+            </tr>
+    		<tr>
+    			<td class="horizontal-space-small"></td>
+    			<td colspan="2">Cepat sekali! Serasa menunggu tikus Cepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikus</td>
+    		</tr>
+    		<tr><td class="vertical-space"></td></tr>
+    	</table>
+    </div>
+    <div class="history-list-item">
+    	<table>
+    		<tr>
+    			<td rowspan="6"><img class="square-image" src="fish.png" alt="Driver Profile"></td>
+    			<td rowspan="6" class="horizontal-space"></td>
+    			<td colspan="2" class="history-date">Sunday, September 24th 2017</td>
+    			<td width="100" rowspan="2"><button class="hide-button">HIDE</button></td>
+    		</tr>
+    		<tr>
+    			<td colspan="2" class="history-driver-name">Bomba-rattata</td>
+    		</tr>
+    		<tr>
+    			<td colspan="2" class="history-route">Saffron City-Pewter City</td>
+    		</tr>
+    		<tr>
+    			<td colspan="2" class="history-rating">gave <font color="orange">5</font> stars for this order</td>
+    		</tr>
+    		<tr>
+    			<td colspan="2">and left comment:</td>
+    		</tr>
+    		<tr>
+    			<td class="horizontal-space-small"></td>
+    			<td colspan="2">Cepat sekali! Serasa menunggu tikus Cepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikusCepat sekali! Serasa menunggu tikus</td>
+    		</tr>
+    	</table>
+    </div>
+</body>
+</html>
