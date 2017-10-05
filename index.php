668c6c549dd0a1c35f1b688e8c1d21fe90d56f9d
catherinealmira <13515111@std.stei.itb.ac.id>
2017-10-05 14:03:18 +0700

    login signup add to db

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+	require 'connection.php';
+	session_start();
+	if ($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		if (isset($_POST['login']))
+		{
+			require 'loginprocess.php';
+		}
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
