d525508c7cbbceed4a2a3dc3fb8f7afbe3777e6b
catherinealmira <13515111@std.stei.itb.ac.id>
2017-10-05 13:09:18 +0700

    tambah tampilan editlocation, editprofile, rename html jadi php

--- index.php
+++ index.php
@@ -1,35 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ojek Panas</title>
+	<link rel="icon" href="icon.png" />
+	<title>Ojek Panas | Login</title>
 	<link rel="stylesheet" type="text/css" href="./css/style.css">
 </head>
 <body>
 	<div id="signin-rectangle">
 		<div class="title"><span>LOGIN</span></div>
-
-		<form method="POST">
+		<script src="js/validation.js"></script>
+		<form method="POST" action="index.php" name="myForm" onsubmit="return validateForm()">
 			<table class="form" border="0">
 				<tr>
 					<td><label class="label" for="username">Username</label></td>
 					<td><input class="text-field" type="text" name="username" id="username" maxlength="20"></td>
-				</tr>	
+				</tr>
 				<tr>
 					<td><label class="label" for="pass">Password</label></td>
 					<td><input class="text-field" type="password" name="pass" id="pass" maxlength="16"></td>
 				</tr>
 			</table>
 			<div class="empty-space"></div>
-
 			<div class="block-container">
-				<div class="no-account"><a href="signup.html">Don't have an account?</a></div>
-				<div class="button noselect">GO!</div>
+				<div class="no-account"><a href="signup.php">Don't have an account?</a></div>
+				<button class="button" name="login">GO!</button>
 			</div>
-
 		</form>
-		
-		
-		
 	</div>
 </body>
 </html>
