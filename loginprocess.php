7a080dba489b538faa7b43903555a13b40acbc96
catherinealmira <13515111@std.stei.itb.ac.id>
2017-10-05 15:59:40 +0700

    login process

---
+++ loginprocess.php
@@ -0,0 +1,15 @@
+<?php
+	$username = $mysqli->real_escape_string($_POST['username']);
+	$result = $mysqli->query("SELECT * FROM user WHERE username='$username'");
+	if ($result->num_rows == 0) {
+		echo "<script>alert('Akun dengan username tersebut belum ada')</script>";
+	} else {
+		$user = $result->fetch_assoc();
+		if (password_verify($_POST['pass'], $user['pass'])) {
+			$_SESSION['id'] = $user['id'];
+			header("location: profile.php?id_active=$_SESSION[id]");
+		} else {
+			echo "<script>alert('Password salah!')</script>";
+		}
+	}
+?>
