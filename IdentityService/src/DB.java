d95e3baaa8f5e855aac079512ec005ac0315b3a7
Erick Wijaya <wijaya.erick52@gmail.com>
2017-11-04 17:42:52 +0700

    login still exception

---
+++ IdentityService/src/DB.java
@@ -0,0 +1,10 @@
+
+public class DB {
+	// JDBC driver name and database URL
+    public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+    public static final String DB_URL="jdbc:mysql://localhost/3306/gaussianlord_acc";
+
+    //  Database credentials
+    public static final String USER = "root";
+    public static final String PASS = ""; 
+}
--- IdentityService/src/Login.java
+++ IdentityService/src/Login.java
@@ -2,6 +2,11 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -15,7 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -30,20 +35,74 @@ public class Login extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		response.setContentType("text/html");
+		/*response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		
 		out.print("<html><body>");
 		out.print("<h2>Hello World</h2>");
-		out.print("</body></html>");
+		out.print("</body></html>");*/
+		doPost(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			Class.forName(DB.JDBC_DRIVER);
+			
+			// Open connection
+			conn = DriverManager.getConnection(DB.DB_URL, DB.USER, DB.PASS);
+			
+			// Execute SQL query
+			pstmt = conn.prepareStatement("SELECT * FROM account WHERE username=? AND password=?");
+			pstmt.setString(1, "erick"/*request.getParameter("username")*/);
+			pstmt.setString(2, "bertus"/*request.getParameter("password")*/);
+			
+			ResultSet rs = pstmt.executeQuery();
+			if (!rs.isBeforeFirst()) {    
+				response.setContentType("text/html");
+				PrintWriter out = response.getWriter();
+				
+				out.print("<html><body>");
+				out.print("<h2>Username Password is not valid</h2>");
+				out.print("</body></html>");
+			} else {
+				rs.next();
+				// Generate Token
+				
+				response.setContentType("text/html");
+				PrintWriter out = response.getWriter();
+				
+				out.print("<html><body>");
+				out.print("<h2>Valid</h2>");
+				out.print("</body></html>");
+			}
 
+	        // Clean-up environment
+	        rs.close();
+	        pstmt.close();
+	        conn.close();
+	        
+	    } catch(SQLException se) {
+	        se.printStackTrace();
+	    } catch(Exception e) {
+	        e.printStackTrace();
+	    } finally {
+	        //finally block used to close resources
+	        try {
+	        	if(pstmt!=null)
+	        		pstmt.close();
+	        } catch(SQLException se2) {}
+	        try {
+	            if(conn!=null)
+	            	conn.close();
+	        } catch(SQLException se) {
+	            se.printStackTrace();
+	        }
+	    }
+	}
 }
