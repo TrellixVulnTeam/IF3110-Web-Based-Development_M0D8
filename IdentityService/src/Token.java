efee553019bb79f84a373ef478f0a235973bfa92
dewitast <dewitast20@gmail.com>
2017-11-04 09:01:14 +0700

    Add generate token

--- IdentityService/src/Token.java
+++ IdentityService/src/Token.java
@@ -1,8 +1,26 @@
+import java.util.Random;
 
 public class Token {
-	private String token;
+	private StringBuilder token;
+	private static final int length = 20;
 	
 	public Token() {
-		
+		token = new StringBuilder(length);
+	}
+	
+	public void generate() {
+		Random rand = new Random();
+		for (int i = 0; i < length; ++i) {
+			int ascii = rand.nextInt(26) + (int) 'a';
+			boolean capital = rand.nextBoolean();
+			if (capital) {
+				ascii -= 32;
+			}
+			token.setCharAt(i, (char) ascii);
+		}
+	}
+	
+	public String get() {
+		return token.toString();
 	}
 }
