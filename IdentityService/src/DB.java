076e932948d4823d93ed7b9cc5d0ed75bcf6910e
Erick Wijaya <wijaya.erick52@gmail.com>
2017-11-04 17:49:26 +0700

    login done

--- IdentityService/src/DB.java
+++ IdentityService/src/DB.java
@@ -2,7 +2,7 @@
 public class DB {
 	// JDBC driver name and database URL
     public static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
-    public static final String DB_URL="jdbc:mysql://localhost/3306/gaussianlord_acc";
+    public static final String DB_URL="jdbc:mysql://localhost/gaussianlord_acc";
 
     //  Database credentials
     public static final String USER = "root";
