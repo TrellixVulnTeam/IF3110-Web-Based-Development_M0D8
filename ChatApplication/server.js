77fdfcd4b2a6c35557c9848df35f4a2bccacedf4
Erick Wijaya <wijaya.erick52@gmail.com>
2017-11-27 19:39:45 +0700

    fcm send message done

--- ChatApplication/server.js
+++ ChatApplication/server.js
@@ -1,6 +1,5 @@
 // set up ======================================================================
 var express = require('express');
-var firebase = require("firebase");
 var app = express(); 						// create our app w/ express
 var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 8080; 				// set the port
@@ -9,6 +8,12 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var admin = require("firebase-admin");
+var serviceAccount = require("C:\\data\\serviceAccountKey.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://tubes-wbd-3-95397.firebaseio.com"
+});
 
 // configuration ===============================================================
 mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
@@ -19,17 +24,6 @@ app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
-// Firebase Config
-var config = {
-	apiKey: "AIzaSyB5rXXTdwUqnTTdaONDxfj845PgYD54384",
-	authDomain: "tubes-wbd-3-95397.firebaseapp.com",
-	databaseURL: "https://tubes-wbd-3-95397.firebaseio.com",
-	projectId: "tubes-wbd-3-95397",
-	storageBucket: "tubes-wbd-3-95397.appspot.com",
-	messagingSenderId: "691072226468"
-};
-firebase.initializeApp(config);
 
 // Set CORS header and intercept "OPTIONS" preflight call from AngularJS
 /*var allowCrossDomain = function(req, res, next) {
@@ -164,33 +158,32 @@ function sendMessage(idSender, tokenReceiver, messageContent){
 function sendInfo(idSender, tokenReceiver, usernameSender){
 	 
 	console.log("Sending info with: id=" + idSender + ", username=" + usernameSender + ", tokenReceiver=" + tokenReceiver);
-	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-	xmlhttp.open("POST", "https://fcm.googleapis.com/fcm/send");
-	xmlhttp.setRequestHeader("Content-Type", "application/json");
-	xmlhttp.setRequestHeader("Authorization", "key=AAAAoOcdVKQ:APA91bHKeEkg_Uhu2VIkmuVJVats98jm3mQ5F3Wa7BJWpAg8svx4yDFAPFvE-czb_fOtej4Kq-oTnm5_Y6vK0_gRRiEgrv4EVcDrFCiqnUtlNDmSkc0W2fze6cpBAqse0p_cxt46LCdM");
-	xmlhttp.send(JSON.stringify({to:tokenReceiver, data: {type:"info", id_sender:idSender, username_sender:usernameSender}}));
 	
-	/*
-	  $.ajax({        
-	            type : 'POST',
-	            crossDomain: true,
-	            url : "https://fcm.googleapis.com/fcm/send",
-	            headers : {
-	                Authorization : 'key=AAAAoOcdVKQ:APA91bHKeEkg_Uhu2VIkmuVJVats98jm3mQ5F3Wa7BJWpAg8svx4yDFAPFvE-czb_fOtej4Kq-oTnm5_Y6vK0_gRRiEgrv4EVcDrFCiqnUtlNDmSkc0W2fze6cpBAqse0p_cxt46LCdM'
-	            },
-	            beforeSend: function (xhrObj) {
-	                xhrObj.setRequestHeader("Content-Type", "application/json");
-	            },
-	            contentType : 'application/json',
-	            dataType: 'json',
-	            data: JSON.stringify({to:tokenReceiver, data: {type:"info", id_sender:idSender, username_sender:usernameSender}}),
-	            success : function(response) {
-	                console.log("success: " + response);
-	            },
-	            error : function(xhr, status, error) {
-	                console.log("error: " + xhr.error);                   
-	            }
-	  });*/
+	// This registration token comes from the client FCM SDKs.
+	var registrationToken = tokenReceiver;
+
+	// See the "Defining the message payload" section below for details
+	// on how to define a message payload.
+	var payload = {
+	  data: {
+		type: "info",
+	    id_sender: idSender,
+	    username_sender: usernameSender
+	  }
+	};
+
+	// Send a message to the device corresponding to the provided
+	// registration token.
+	admin.messaging().sendToDevice(registrationToken, payload)
+	  .then(function(response) {
+	    // See the MessagingDevicesResponse reference documentation for
+	    // the contents of response.
+	    console.log("Successfully sent message:", response);
+	  })
+	  .catch(function(error) {
+	    console.log("Error sending message:", error);
+	  });
+	
 }
 
 // routes ======================================================================
